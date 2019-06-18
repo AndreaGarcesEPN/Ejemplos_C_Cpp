22e7d7359ba76a2a7fd72c610813b057cb58eab9
AndreaGarcesEPN <50632356+AndreaGarcesEPN@users.noreply.github.com>
2019-06-18 11:35:05 -0500

    Primer Ejemplo

---
+++ Estructuras/Ejemplo1_Estructuras.cpp
@@ -0,0 +1,18 @@
+#include<cstdlib>
+#include<iostream>
+
+using namespace std;
+
+struct Cliente
+{
+	char nombre[20];
+	int numUnidades;
+	float precio;
+	char estado;
+};
+
+int main()
+{
+	system("PAUSE");
+	return EXIT_SUCCESS;
+}
